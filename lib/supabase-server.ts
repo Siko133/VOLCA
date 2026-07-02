96ed96e2918b4b2f0f9a4e546cdea360bd4d5fd0
Siko133 <elsayedalaa566@gmail.com>
2026-07-02 08:17:00 +0300

    debug supabase

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,5 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL =", process.env.SUPABASE_URL);
+console.log(
+  "KEY EXISTS =",
+  !!process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
 export const supabaseServer = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
